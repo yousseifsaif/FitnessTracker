f6d409c66d961cc34c748a40b03591de9bbaa372
yousseifsaif <yousseifsaif@users.noreply.github.com>
2025-01-06 23:09:48 +0200

    .

--- app/src/main/java/com/example/fitnesstracker/OnBoarding1.kt
+++ app/src/main/java/com/example/fitnesstracker/OnBoarding1.kt
@@ -6,11 +6,11 @@ import android.os.Handler
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
-class SplashScreen : AppCompatActivity() {
+class OnBoarding1 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_splash_screen)
+        setContentView(R.layout.activity_on_boarding_1)
         Handler().postDelayed({
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
