64dfb4bc30d17d654e884fe3df52fa6b42265b68
yousseifsaif <yousseifsaif@users.noreply.github.com>
2025-01-13 19:26:54 +0200

    .

--- app/src/main/java/com/example/fitnesstracker/Boarding/Log_in.kt
+++ app/src/main/java/com/example/fitnesstracker/Boarding/Log_in.kt
@@ -1,26 +1,21 @@
 package com.example.fitnesstracker.Boarding
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.fitnesstracker.R
 
-class Boarding4 : AppCompatActivity() {
+class Log_in : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_boarding4)
-        val btn=findViewById<Button>(R.id.btn3)
-        btn.setOnClickListener {
-            val intent = Intent(this, Log_in::class.java)
-            startActivity(intent)
-
+        setContentView(R.layout.activity_log_in)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
         }
-
-
     }
 }
