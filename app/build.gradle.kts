66d7393e731253eb0a1cac6d40b33429324f8fb8
yousseifsaif <yousseifsaif@users.noreply.github.com>
2025-01-15 02:08:31 +0200

    .

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -53,4 +54,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation(platform("com.google.firebase:firebase-bom:33.7.0"))
+    implementation("com.google.firebase:firebase-analytics")
 }
--- app/src/main/java/com/example/fitnesstracker/Boarding/Boarding4.kt
+++ app/src/main/java/com/example/fitnesstracker/Boarding/Boarding4.kt
@@ -3,6 +3,7 @@ package com.example.fitnesstracker.Boarding
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.example.fitnesstracker.Login_SignUp.LogIn
