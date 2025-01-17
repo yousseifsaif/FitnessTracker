0b3bbae448963e005073137cec0d68b2f8ae42b4
yousseifsaif <yousseifsaif@users.noreply.github.com>
2025-01-17 18:48:31 +0200

    .

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,4 +59,7 @@ dependencies {
     implementation(platform("com.google.firebase:firebase-bom:33.7.0"))
     implementation("com.google.firebase:firebase-analytics")
     implementation ("com.google.firebase:firebase-database:21.0.0")
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.android.gms:play-services-auth:21.3.0")
+
 }
--- app/src/main/java/com/example/fitnesstracker/Login_SignUp/SignUp.kt
+++ app/src/main/java/com/example/fitnesstracker/Login_SignUp/SignUp.kt
@@ -11,43 +11,52 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.fitnesstracker.R
 import com.example.fitnesstracker.setup_pages.SetupPage
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 
 class SignUp : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
-
+    private lateinit var database: DatabaseReference
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        auth = FirebaseAuth.getInstance()
+        database = FirebaseDatabase.getInstance().getReference("Users")
+
         setContentView(R.layout.activity_sign_up)
-        val text=findViewById<TextView>(R.id.text)
-        val emailInput = findViewById<EditText>(R.id.EmailEditText)
+        val text = findViewById<TextView>(R.id.text)
+        val usernameInput = findViewById<EditText>(R.id.usernamelEditText)
         val passwordInput = findViewById<EditText>(R.id.passwordEditText)
         val confirmPasswordInput = findViewById<EditText>(R.id.editText2)
-        val mobileInput = findViewById<EditText>(R.id.mobileEditText)
+        val emailInput = findViewById<EditText>(R.id.emailEditText)
         val registerButton = findViewById<Button>(R.id.signup_button)
-text.setOnClickListener{
- var   intentText=Intent(this,LogIn::class.java)
-    startActivity(intentText)
-    finish()
-}
+        text.setOnClickListener {
+            val intenttext = Intent(this, LogIn::class.java)
+            startActivity(intenttext)
+        }
 
         registerButton.setOnClickListener {
-            val email = emailInput.text.toString().trim()
+            val userName = emailInput.text.toString().trim()
             val password = passwordInput.text.toString().trim()
             val confirmPassword = confirmPasswordInput.text.toString().trim()
-            val mobile = mobileInput.text.toString().trim()
-
-            if (email.isNotEmpty() && password.isNotEmpty()) {
+            val email = emailInput.text.toString().trim()
+            if (password != confirmPassword) {
+                Toast.makeText(this, "Passwords do not match", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            if (email.isNotEmpty() && password.isNotEmpty() && confirmPassword.isNotEmpty() && userName.isNotEmpty()) {
                 auth.createUserWithEmailAndPassword(email, password)
                     .addOnCompleteListener { task ->
                         if (task.isSuccessful) {
+                            val userId = auth.currentUser?.uid
                             Toast.makeText(this, "Registration Successful!", Toast.LENGTH_SHORT)
                                 .show()
                             intent = Intent(this, SetupPage::class.java)
                             startActivity(intent)
+
                         } else {
                             Toast.makeText(
                                 this,
@@ -60,5 +69,6 @@ text.setOnClickListener{
                 Toast.makeText(this, "Please fill in all fields", Toast.LENGTH_SHORT).show()
             }
         }
-        }
+
     }
+}
