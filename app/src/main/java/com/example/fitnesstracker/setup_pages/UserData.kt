e20dcc53f57902e5bee334c6232570e4d5a2d0bc
abdelrhman1002 <abdelrhmanshabana999@gmail.com>
2025-01-30 21:54:47 +0200

    Update UserData.kt

--- app/src/main/java/com/example/fitnesstracker/setup_pages/UserData.kt
+++ app/src/main/java/com/example/fitnesstracker/setup_pages/UserData.kt
@@ -1,10 +1,12 @@
 package com.example.fitnesstracker.setup_pages
 
-class UserData {
-    var name:String?=null
-    var age:String?=null
-    var gender:String?=null
-    var height:Double? = null
-    var weight:Double?=null
-
-}
+class UserData(
+    val name: String = "",
+    val email : String = "",
+    val password : String = "",
+    val age: Int = 0,
+    val gender: String = "",
+    val height: Double = 0.0,
+    val weight: Double = 0.0,
+    val Id : String = ""
+    )
