25f5d3f5f9ef1a402db3dcaf5a166f43fad40655
yousseif saif <prcomputer2@gmail.com>
2025-02-24 16:37:24 +0200

    Make suggestion workout,add workout manual and delete it ,used shared prefrence

--- app/src/main/java/com/example/fitnesstracker/NavigationApp/MealsFragment.kt
+++ app/src/main/java/com/example/fitnesstracker/NavigationApp/MealsFragment.kt
@@ -7,7 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.fitnesstracker.databinding.FragmentMealsBinding
 
-class SettingsFragment : Fragment() {
+class MealsFragment : Fragment() {
     private var _binding: FragmentMealsBinding? = null
     private val binding get() = _binding!!
 
