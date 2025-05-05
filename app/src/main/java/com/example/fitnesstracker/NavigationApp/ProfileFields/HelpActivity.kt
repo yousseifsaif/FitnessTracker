6d3d5e526a35cf736347995349a5d45282076b44
yousseif saif <prcomputer2@gmail.com>
2025-05-05 19:24:21 +0300

    change somethings

--- app/src/main/java/com/example/fitnesstracker/NavigationApp/ProfileFields/HelpActivity.kt
+++ app/src/main/java/com/example/fitnesstracker/NavigationApp/ProfileFields/HelpActivity.kt
@@ -12,10 +12,6 @@ class HelpActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_help)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
     }
 }
--- app/src/main/java/com/example/fitnesstracker/NavigationApp/ProfileFields/PrivacyPolicy.kt
+++ app/src/main/java/com/example/fitnesstracker/NavigationApp/ProfileFields/PrivacyPolicy.kt
@@ -12,10 +12,5 @@ class PrivacyPolicy : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_privacy_policy)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
     }
 }
